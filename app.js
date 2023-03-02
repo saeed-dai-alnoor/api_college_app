91f54c93c2e3d279c41edb63a353e81129297070
saeed-dai-alnoor <saeeddaialnoor@gmail.com>
2023-03-02 17:00:31 +0200

    m

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 // const config = require('./config/config.json');
 
 // to user routes 
@@ -14,6 +14,7 @@ app.use('/api', levelRouter)
 
 // port
 const port = process.env.PORT || 5000;
+const connection = new Sequelize('postgres://api-college-app-main-db-0697db994aab86e91:PzvQsTqDvJScJuagu229H6kFmGjNs6@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/api-college-app-main-db-0697db994aab86e91')
 
 //** */ To connecto with database and run the  server **//
 // All required for connection to database
@@ -24,15 +25,15 @@ const port = process.env.PORT || 5000;
 // const connection = new Sequelize(databaseName, userName,
 //     userPassword, { dialect: dialect });
 
-// connection.authenticate()
-//     .then(() => {
-//         console.log('Nice! Database synced...');
-//         app.listen(PORT, () => {
-//             console.log(`Server running on http://localhost:${PORT}/api`);
-//         });
-//     })
-//     .catch((error) => { console.error('Error connecting' + error); });
+connection.authenticate()
+    .then(() => {
+        console.log('Nice! Database synced...');
+        app.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}/api`);
+        });
+    })
+    .catch((error) => { console.error('Error connecting' + error); });
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`App listening at http://localhost:${port}`);
+// });
