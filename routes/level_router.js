ed232614e9e10c6ae2eb3628d262b1f31144f9f6
saeed-dai-alnoor <saeeddaialnoor@gmail.com>
2023-03-02 17:09:18 +0200

    m

--- routes/level_router.js
+++ routes/level_router.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const LevelRouter = require('../controllers/level_controller');
 const checkToken = require('../auth/token_vaildation');
 
-router.post('/create-level', checkToken, LevelRouter.createLevel);
+router.post('/create-level', LevelRouter.createLevel);
 router.get('/levels', LevelRouter.getLevels);
-router.get('/levels/:levelId', checkToken, LevelRouter.getLevelById);
-router.put('/update-level', checkToken, LevelRouter.updateLevel);
-router.delete('/delete-level', checkToken, LevelRouter.deleteLevel);
+router.get('/levels/:levelId', LevelRouter.getLevelById);
+router.put('/update-level', LevelRouter.updateLevel);
+router.delete('/delete-level', LevelRouter.deleteLevel);
 module.exports = router;
